19514a08c25e1d072c6314d6ac898b175a725a3c
trishnakalita660 <trishnakalita660@gmail.com>
2021-03-02 19:19:38 +0530

    Updates

---
+++ precache-manifest.14e33089da2cc52b3be687ff451fa850.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee73738baa6fea7530f9e4d45a28134b",
+    "url": "/amazon-clone/index.html"
+  },
+  {
+    "revision": "ecf8447066130dc5fe4e",
+    "url": "/amazon-clone/static/css/main.221e7ba7.chunk.css"
+  },
+  {
+    "revision": "1603b7be65ca2b9fb901",
+    "url": "/amazon-clone/static/js/2.34c71030.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/amazon-clone/static/js/2.34c71030.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecf8447066130dc5fe4e",
+    "url": "/amazon-clone/static/js/main.6a129ca6.chunk.js"
+  },
+  {
+    "revision": "3101b3b2c4581d0f9087",
+    "url": "/amazon-clone/static/js/runtime-main.1cf4aa72.js"
+  }
+]);
